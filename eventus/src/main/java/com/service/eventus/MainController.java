902044f31ea02c697ed0ffeafd1b2187c11620f6
이진성 <65940253+ikeener@users.noreply.github.com>
2022-11-29 17:12:53 +0900

    이미지파일 추가

--- eventus/src/main/java/com/service/eventus/MainController.java
+++ eventus/src/main/java/com/service/eventus/MainController.java
@@ -5,13 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping(value="/")
-	public String page() {
-		return "index.html";
-	}
 
-	@GetMapping(value="/Main")
-	public String main() {
+    // 최초 진입 시 실행 페이지
+	@GetMapping(value="/")
+	public String page() {
 		return "main";
 	}
+
 }
