83ca266bba6314f63d933f3cd0a4723548e2ef55
이진성 <65940253+ikeener@users.noreply.github.com>
2022-12-05 16:56:25 +0900

    page 링크 및 mysql 연결

--- eventus/src/main/java/com/service/eventus/MainController.java
+++ eventus/src/main/java/com/service/eventus/MainController.java
@@ -12,9 +12,46 @@ public class MainController {
 		return "login";
 	}
 
-	@GetMapping(value="/join")
+	// 회원가입 약관동의 페이지
+	@GetMapping(value="/join1")
 	public String join() {
 		return "join1";
 	}
 
+	@GetMapping(value="/join2")
+	public String join2() { return "join2"; }
+
+	@GetMapping(value="/join3")
+	public String join3() { return "join3"; }
+
+	// 아이디 비멀번호 관련 페이지
+	@GetMapping(value="/find_id_pw")
+	public String find_id_pw() { return "find_id_pw"; }
+
+	@GetMapping(value="/changing_pw")
+	public String changing_pw() { return "changing_pw"; }
+
+	@GetMapping(value="/changed_pw")
+	public String changed_pw() { return "changed_pw"; }
+
+	// main 페이지
+	@GetMapping(value="/main")
+	public String Main() {
+		return "main";
+	}
+
+	@GetMapping(value="/manage_event")
+	public String manage_event() {
+		return "manage_event";
+	}
+
+	@GetMapping(value="/manage_staff")
+	public String manage_staff() {
+		return "manage_staff";
+	}
+
+	@GetMapping(value="report_work")
+	public String report_work() {
+		return "report_work";
+	}
 }
