8411d1c1fa5d4ff817bc06579fc9ebdca9517433
이진성 <65940253+ikeener@users.noreply.github.com>
2023-01-05 16:57:22 +0900

    근로계약서 다운 작성 중

--- eventus/src/main/java/com/service/eventus/master/MasterService.java
+++ eventus/src/main/java/com/service/eventus/master/MasterService.java
@@ -1,10 +1,20 @@
 package com.service.eventus.master;
 
+import com.lowagie.text.DocumentException;
 import com.service.eventus.mappers.MasterDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class MasterService {
@@ -18,5 +28,35 @@ public class MasterService {
 
     public List<MasterVo> getListUserApp(String user_id) {
         return masterDao.getListUserApp(user_id);
+    }
+
+    // pdf 다운로드
+    private Logger logger = LoggerFactory.getLogger(MasterService.class);
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${pdf.directory}")
+    private String pdfDirectory;
+
+    public void generatePdfFile(String templateName, Map<String, Object> data, String pdfFileName) {
+        Context context = new Context();
+        context.setVariables(data);
+
+        String htmlContent = templateEngine.process(templateName, context);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(pdfDirectory + pdfFileName);
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(htmlContent);
+            renderer.layout();
+            renderer.createPDF(fileOutputStream, false);
+            renderer.finishPDF();
+
+
+        } catch (FileNotFoundException e) {
+            logger.error(e.getMessage(), e);
+        } catch (DocumentException e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 }
