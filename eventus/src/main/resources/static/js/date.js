b73fb4d6c45df226facb67fcefa9b6197ea298a4
yukyzoe8999 <wlikujo@gmail.com>
2023-01-17 16:34:01 +0900

    근무기록 시간 수정(진행중)

--- eventus/src/main/resources/static/js/date.js
+++ eventus/src/main/resources/static/js/date.js
@@ -10,9 +10,9 @@ $(document).ready(function() {
 		/*         changeMonth: true,
 				 changeYear: true,
 				 showMonthAfterYear: true,*/
-/*		dayNamesMin: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],*/
+		/*		dayNamesMin: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],*/
 		dateFormat: 'yy.mm.dd',
-		
+
 	});
 
 	$('#s_calendar').datepicker("option", "maxDate", $("#e_calendar").val());
@@ -24,6 +24,56 @@ $(document).ready(function() {
 	$('#e_calendar').datepicker("option", "minDate", $("#s_calendar").val());
 	$('#e_calendar').datepicker("option", "onClose", function(selectedDate) {
 		$("#s_calendar").datepicker("option", "maxDate", selectedDate);
+	});
+
+
+});
+
+$(document).ready(function() {
+
+	$("[id='update_btn']").click(function() {
+		
+		var num = $(this).attr('value');
+		var update_btn_num = "li."+num;
+		
+		$(update_btn_num).siblings(".list_start_area").css({ "display": "none" });
+		$(update_btn_num).siblings(".list_outing_area").css({ "display": "none" });
+		$(update_btn_num).siblings(".list_comeback_area").css({ "display": "none" });
+		$(update_btn_num).siblings(".list_end_area").css({ "display": "none" });
+		$(update_btn_num).siblings(".time_update").css({ "display": "inline" });
+		
+		$(update_btn_num).siblings(".input_wrap").css({ "display": "flex" });
+		
+		$(update_btn_num).children().children().children(".pen_btn").css({ "display": "none" });
+		
+		$(update_btn_num).children("#check_btn").css({ "display": "inline" });
+		$(update_btn_num).children("#close_btn").css({ "display": "inline" });
+
+
+	});
+	
+	$("[id='close_btn']").click(function() {
+		
+		var num = $(this).attr('value');
+		var update_btn_num = "li."+num;
+		
+		$(update_btn_num).siblings("#start_area_input").css({ "display": "none" });
+		$(update_btn_num).siblings("#outing_area_input").css({ "display": "none" });
+		$(update_btn_num).siblings("#comeback_area_input").css({ "display": "none" });
+		$(update_btn_num).siblings("#end_area_input").css({ "display": "none" });
+		$(update_btn_num).siblings(".list_start_area").css({ "display": "inline" });
+		$(update_btn_num).siblings(".list_outing_area").css({ "display": "inline" });
+		$(update_btn_num).siblings(".list_comeback_area").css({ "display": "inline" });
+		$(update_btn_num).siblings(".list_end_area").css({ "display": "inline" });
+		
+		$(update_btn_num).siblings(".input_wrap").css({ "display": "flex" });
+		
+		$(update_btn_num).children().children().children(".pen_btn").css({ "display": "inline" });
+		
+		$(update_btn_num).children().children().children(".check_btn").css({ "display": "none" });
+		$(update_btn_num).children().children().children(".close_btn").css({ "display": "none" });
+
+
 	});
 
 
