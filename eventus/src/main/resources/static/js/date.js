68bcd88c4887653eca7baf5359ec6ef0517a97d5
yukyzoe8999 <wlikujo@gmail.com>
2023-01-20 09:17:21 +0900

    근무기록 css 수정

--- eventus/src/main/resources/static/js/date.js
+++ eventus/src/main/resources/static/js/date.js
@@ -57,10 +57,11 @@ $(document).ready(function() {
 		var num = $(this).attr('value');
 		var update_btn_num = "li."+num;
 		
-		$(update_btn_num).siblings("#start_area_input").css({ "display": "none" });
-		$(update_btn_num).siblings("#outing_area_input").css({ "display": "none" });
-		$(update_btn_num).siblings("#comeback_area_input").css({ "display": "none" });
-		$(update_btn_num).siblings("#end_area_input").css({ "display": "none" });
+		/*$(update_btn_num).siblings().children("#start_area_input").css({ "display": "none" });
+		$(update_btn_num).siblings().children("#outing_area_input").css({ "display": "none" });
+		$(update_btn_num).siblings().children("#comeback_area_input").css({ "display": "none" });
+		$(update_btn_num).siblings().children("#end_area_input").css({ "display": "none" });*/
+		$(update_btn_num).siblings(".time_update").css({ "display": "none" });
 		$(update_btn_num).siblings(".list_start_area").css({ "display": "inline" });
 		$(update_btn_num).siblings(".list_outing_area").css({ "display": "inline" });
 		$(update_btn_num).siblings(".list_comeback_area").css({ "display": "inline" });
@@ -70,8 +71,8 @@ $(document).ready(function() {
 		
 		$(update_btn_num).children().children().children(".pen_btn").css({ "display": "inline" });
 		
-		$(update_btn_num).children().children().children(".check_btn").css({ "display": "none" });
-		$(update_btn_num).children().children().children(".close_btn").css({ "display": "none" });
+		$(update_btn_num).children("#check_btn").css({ "display": "none" });
+		$(update_btn_num).children("#close_btn").css({ "display": "none" });
 
 
 	});
